51de2018cada73eb34f9c3d981b41f804c71543a
jordi110398 <45258880+jordi110398@users.noreply.github.com>
2025-07-18 14:32:14 +0200

    panel controls

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public void Play() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    public GameObject controlsImage;
+
+    void Update()
+    {
+        if (controlsImage != null && controlsImage.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            controlsImage.SetActive(false);
+        }
     }
 
-    public void Exit() {
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
         Application.Quit();
     }
 
+    public void ShowControls()
+    {
+        if (controlsImage != null)
+            controlsImage.SetActive(true);
+    }
 }
--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -5,9 +5,19 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     public GameObject buttonToSelect;
+    public GameObject controlsPanel;
     public Toggle musicToggle;
     public Toggle parallaxToggle;
     public ParallaxBackground parallaxBackground;
+
+    void Update()
+    {
+        if (controlsPanel != null && controlsPanel.activeSelf &&
+            (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton1)))
+        {
+            controlsPanel.SetActive(false);
+        }
+    }
 
     public void Start()
     {
@@ -19,5 +29,9 @@ public class OptionsMenu : MonoBehaviour
             EventSystem.current.SetSelectedGameObject(buttonToSelect);
     }
 
-
+    public void ShowControlsPanel()
+    {
+        if (controlsPanel != null)
+            controlsPanel.SetActive(true);
+    }
 }
